20c05488324d8a50c01783b072560c5c6dfb0642
SadikDindwar <112097982+SadikDindwar@users.noreply.github.com>
2024-01-22 00:55:41 +0530

    pushed updates on 1/22/2024

--- com.tunehub/src/main/java/com/example/demo/controller/UsersController.java
+++ com.tunehub/src/main/java/com/example/demo/controller/UsersController.java
@@ -1,5 +1,57 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entities.Users;
+import com.example.demo.services.UsersService;
+
+@Controller
 public class UsersController {
+
+//	@PostMapping("/register")
+//	public String addUsers(@RequestParam("username")String username,
+//			@RequestParam("email")String email,
+//			@RequestParam("password")String password,
+//			@RequestParam("gender")String gender,
+//			@RequestParam("role")String role,
+//			@RequestParam("address")String address		
+//			) {		
+//		System.out.println(username+" "+email+" "+password);
+
+	@Autowired
+	UsersService service;
+
+	@PostMapping("/register")
+	public String addUsers(@ModelAttribute Users user) {
+		boolean userStatus = service.emailExists(user.getEmail());
+		if (userStatus == false) {
+			service.addUser(user);
+			System.out.println("User added");
+
+		} else {
+			System.out.println("User already exists");
+		}
+
+		return "home";
+	}
+
+	@PostMapping("/validate")
+	public String validate(@RequestParam("email") String email, @RequestParam("password") String password) {
+		if (service.validateUser(email, password) == true) {
+			String role = service.getRole(email);
+			
+			if (role.equals("customer") ) {
+				return "customerHome";
+			} else {
+				return "adminHome";
+			}
+		} else {
+			return "login";
+		}
+	}
 
 }
--- com.tunehub/src/main/java/com/example/demo/entities/Users.java
+++ com.tunehub/src/main/java/com/example/demo/entities/Users.java
@@ -1,11 +1,14 @@
 package com.example.demo.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Users {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	int id;
 	String username;
 	String email;
