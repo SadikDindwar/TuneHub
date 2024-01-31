ebb75381641a81692af975971f3beac402265e13
SadikDindwar <112097982+SadikDindwar@users.noreply.github.com>
2024-01-31 21:16:03 +0530

    updated

--- com.tunehub/src/main/java/com/example/demo/repositories/SongRepository.java
+++ com.tunehub/src/main/java/com/example/demo/repositories/SongRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entities.Song;
 
 public interface SongRepository extends JpaRepository<Song, Integer> {
-	public Song findByName(String Name); 
+	public Song findByName(String Name);
 
 }
