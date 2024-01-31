12dc0a22a703a6a9a14b71bb21ccbc51a18b12bc
SadikDindwar <112097982+SadikDindwar@users.noreply.github.com>
2024-01-31 21:13:26 +0530

    updated

--- com.tunehub/src/main/java/com/example/demo/entities/Users.java
+++ com.tunehub/src/main/java/com/example/demo/entities/Users.java
@@ -17,12 +17,10 @@ public class Users {
 	String role;
 	String address;
 	boolean isPremium;
-	
 
 	public Users() {
 		super();
 	}
-
 
 	public Users(int id, String username, String email, String password, String gender, String role, String address,
 			boolean isPremium) {
@@ -37,95 +35,74 @@ public class Users {
 		this.isPremium = isPremium;
 	}
 
-
 	public int getId() {
 		return id;
 	}
-
 
 	public void setId(int id) {
 		this.id = id;
 	}
 
-
 	public String getUsername() {
 		return username;
 	}
-
 
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
-
 	public String getEmail() {
 		return email;
 	}
-
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
 	public String getPassword() {
 		return password;
 	}
-
 
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-
 	public String getGender() {
 		return gender;
 	}
-
 
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
 
-
 	public String getRole() {
 		return role;
 	}
-
 
 	public void setRole(String role) {
 		this.role = role;
 	}
 
-
 	public String getAddress() {
 		return address;
 	}
-
 
 	public void setAddress(String address) {
 		this.address = address;
 	}
 
-
 	public boolean isPremium() {
 		return isPremium;
 	}
 
-
 	public void setPremium(boolean isPremium) {
 		this.isPremium = isPremium;
 	}
-
 
 	@Override
 	public String toString() {
 		return "Users [id=" + id + ", username=" + username + ", email=" + email + ", password=" + password
 				+ ", gender=" + gender + ", role=" + role + ", address=" + address + ", isPremium=" + isPremium + "]";
 	}
-	
-	
-
-	
 
 }
